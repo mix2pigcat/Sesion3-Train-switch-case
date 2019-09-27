5917b0ffe4aad9b36f5fdeb8e5c146114627f2b5
thanhle <mix2pigcat@gmaail.com>
2019-09-27 18:24:13 +0700

    file bai1

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+switch (browser) {
+    case 'Edge':
+        alert("you've got the Edge!");
+        break;
+    case 'Chorme':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+        alert('Okay we support these browsers too');
+        break;
+    default:
+        alert('We hope that thus page looks ok!')
+}
